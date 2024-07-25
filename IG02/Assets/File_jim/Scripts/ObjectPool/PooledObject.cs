e6d7a9776d2873dff28612ff532edad27477b9a4
jimu0 <Gigtdty90@126.com>
2024-07-26 02:12:56 +0800

    fix：简单整理文件夹

---
+++ IG02/Assets/File_jim/Scripts/ObjectPool/PooledObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UITemplate.File_jim.Scripts.ObjectPool
+{
+    public struct PooledObject<T> : IDisposable where T : class
+    {
+        private readonly T m_ToReturn;
+        private readonly IObjectPool<T> m_Pool;
+
+        internal PooledObject(T value, IObjectPool<T> pool)
+        {
+            this.m_ToReturn = value;
+            this.m_Pool = pool;
+        }
+
+        void IDisposable.Dispose() => this.m_Pool.Release(this.m_ToReturn);
+    }
+}
---
+++ IG02/Assets/File_jim/Scripts/ObjectPool/poolTest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace UITemplate.File_jim.Scripts.ObjectPool
+{
+    public class PoolTest : MonoBehaviour
+    {
+        private ObjectPool<GameObject> Pool;
+        void Start()
+        {
+            Pool = new ObjectPool<GameObject>(OnCreate, OnGet, OnRelease, OnDestory,
+                true, 10, 1000);
+        }
+        GameObject OnCreate()
+        {
+            return new GameObject("wokao");
+        }
+        void OnGet(GameObject gameObject)
+        {
+            Debug.Log("Onget");
+        }
+        void OnRelease(GameObject gameObject)
+        {
+            Debug.Log("OnRelease");
+        }
+        void OnDestory(GameObject gameObject)
+        {
+            Debug.Log("OnDestory");
+        }
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Pool.Get();
+            }
+        }
+    }
+}
