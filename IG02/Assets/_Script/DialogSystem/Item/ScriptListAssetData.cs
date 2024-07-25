4705297b2f4b296ed93e2927e551f2911531982a
jimu0 <Gigtdty90@126.com>
2024-07-25 19:51:44 +0800

    fix：using UnityEditor.Progress;导致无法打包，注释掉

--- IG02/Assets/_Script/DialogSystem/Item/ScriptListAssetData.cs
+++ IG02/Assets/_Script/DialogSystem/Item/ScriptListAssetData.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 public class ScriptListAssetData : ScriptableBase<ScriptListAssetData, ItemContainer>
 {
